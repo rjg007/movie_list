2f7a159343c853ddb994f72874ca3ce43939729b
Rahul Ghorpade <rghorpade1995@gmail.com>
2021-03-25 10:28:34 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BiCameraMovie} from 'react-icons/bi'
+import {MdLocalMovies} from 'react-icons/md'
+import MoviesContainer from './components/MoviesContainer'
+import './App.css'
 
-function App() {
+const App = (props) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid' >
+      <div className='app_div'>
+        <h1 className='main_title'> <BiCameraMovie /> My Big Movie List <MdLocalMovies /> </h1>
+        <MoviesContainer />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
